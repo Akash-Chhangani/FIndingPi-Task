c8932442b91e5307eb56c2e5407b2e94a7dd6404
Akash-Chhangani <akashchhangani4@gmail.com>
2024-06-26 23:56:11 +0530

    Baack Ground Image is decide. All the thoughts which will display on screen are ready in data.js file

---
+++ src/data.js
@@ -0,0 +1,169 @@
+const goodThoughts = [
+  {
+    id: 1,
+    authorName: "Rabindranath Tagore",
+    quote:
+      "You can't cross the sea merely by standing and staring at the water.",
+  },
+  {
+    id: 2,
+    authorName: "Mahatma Gandhi",
+    quote: "Be the change that you wish to see in the world.",
+  },
+  {
+    id: 3,
+    authorName: "Swami Vivekananda",
+    quote: "Arise, awake, and stop not till the goal is reached.",
+  },
+  {
+    id: 4,
+    authorName: "A. P. J. Abdul Kalam",
+    quote:
+      "Dream, dream, dream. Dreams transform into thoughts and thoughts result in action.",
+  },
+  {
+    id: 5,
+    authorName: "Sardar Vallabhbhai Patel",
+    quote:
+      "Manpower without unity is not a strength unless it is harmonized and united properly, then it becomes a spiritual power.",
+  },
+  {
+    id: 6,
+    authorName: "Sarojini Naidu",
+    quote:
+      "We want deeper sincerity of motive, a greater courage in speech and earnestness in action.",
+  },
+  {
+    id: 7,
+    authorName: "Jawaharlal Nehru",
+    quote:
+      "Time is not measured by the passing of years but by what one does, what one feels, and what one achieves.",
+  },
+  {
+    id: 8,
+    authorName: "Dr. B. R. Ambedkar",
+    quote: "Life should be great rather than long.",
+  },
+  {
+    id: 9,
+    authorName: "Indira Gandhi",
+    quote: "The power to question is the basis of all human progress.",
+  },
+  {
+    id: 10,
+    authorName: "Vikram Sarabhai",
+    quote:
+      "He who can listen to the music in the midst of noise can achieve great things.",
+  },
+  {
+    id: 11,
+    authorName: "Amartya Sen",
+    quote:
+      "Development is about transforming the lives of people, not just transforming economies.",
+  },
+  {
+    id: 12,
+    authorName: "Ravi Shankar",
+    quote:
+      "In our culture, we have such respect for musical instruments, they are like part of God.",
+  },
+  {
+    id: 13,
+    authorName: "Mother Teresa",
+    quote:
+      "Spread love everywhere you go. Let no one ever come to you without leaving happier.",
+  },
+  {
+    id: 14,
+    authorName: "J. R. D. Tata",
+    quote: "Uncommon thinkers reuse what common thinkers refuse.",
+  },
+  {
+    id: 15,
+    authorName: "Homi J. Bhabha",
+    quote: "Science has made us gods even before we are worthy of being men.",
+  },
+  {
+    id: 16,
+    authorName: "Satyajit Ray",
+    quote:
+      "The only solutions that are ever worth anything are the solutions that people find themselves.",
+  },
+  {
+    id: 17,
+    authorName: "Amitabh Bachchan",
+    quote:
+      "Bad luck either destroys you or makes you the man or woman you really are.",
+  },
+  {
+    id: 18,
+    authorName: "Lata Mangeshkar",
+    quote: "Music is the voice of the soul.",
+  },
+  {
+    id: 19,
+    authorName: "Ratan Tata",
+    quote:
+      "Take the stones people throw at you and use them to build a monument.",
+  },
+  {
+    id: 20,
+    authorName: "Anupam Kher",
+    quote: "You can't choose the ways in which you'll be tested.",
+  },
+  {
+    id: 21,
+    authorName: "Priyanka Chopra",
+    quote:
+      "Do not try to squeeze into a glass slipper. Instead, shatter the glass ceiling.",
+  },
+  {
+    id: 22,
+    authorName: "Narendra Modi",
+    quote: "Hard work never brings fatigue. It brings satisfaction.",
+  },
+  {
+    id: 23,
+    authorName: "Arundhati Roy",
+    quote:
+      "Another world is not only possible, she is on her way. On a quiet day, I can hear her breathing.",
+  },
+  {
+    id: 24,
+    authorName: "Sachin Tendulkar",
+    quote: "People throw stones at you and you convert them into milestones.",
+  },
+  {
+    id: 25,
+    authorName: "Sania Mirza",
+    quote:
+      "Don't be afraid of making mistakes. They are a part of the learning process.",
+  },
+  {
+    id: 26,
+    authorName: "Sunil Gavaskar",
+    quote: "When you are fit, you feel as if you can do everything.",
+  },
+  {
+    id: 27,
+    authorName: "Kapil Dev",
+    quote:
+      "Apart from education, you need good health, and for that, you need to play sports.",
+  },
+  {
+    id: 28,
+    authorName: "Milkha Singh",
+    quote:
+      "Life is full of struggles. If you persevere and work hard, success will be yours.",
+  },
+  {
+    id: 29,
+    authorName: "Virat Kohli",
+    quote: "Self-belief and hard work will always earn you success.",
+  },
+  {
+    id: 30,
+    authorName: "PV Sindhu",
+    quote: "A true champion works hard and never loses sight of her dreams.",
+  },
+];
